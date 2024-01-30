812ab31dfa1c17ad06fb4a8e2ed750fc3f84ddbf
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2024-01-30 21:28:27 +0100

    React Set Up

---
+++ web-app/src/App.js
@@ -0,0 +1,14 @@
+//import './component/Light';
+import LED from "./component/LED"
+
+
+function App() {
+  
+   // Return the LED component
+   return (
+    <LED />
+  );
+
+}
+
+export default App;
---
+++ web-app/src/component/LED.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styles from '../css/LED.css';
+
+function LED() {
+  return (
+    <p>Hey! Kassem and Yana were here!</p>  
+  );
+}
+
+export default LED;
