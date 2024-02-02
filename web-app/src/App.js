d1303794955cae718c2ea74cc06fe9acbdd7a2f5
Asom98 <kaseemsh985@gmail.com>
2024-02-02 16:48:03 +0100

    adding react router and applying it

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+
+
 import { Home } from "./pages/Home";
+import Lightings from "./pages/Lightings";
 
 function App() {
    return (
-    
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/home" element={<Home  />} />
+        <Route path="/lightings" element={<Lightings />} />
+      </Routes>
+    </BrowserRouter>
   );
 
 }
