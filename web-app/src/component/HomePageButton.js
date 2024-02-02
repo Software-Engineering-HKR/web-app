fa7b8c7b3b533ec6220adc287e7e45f51a6c86ea
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2024-02-02 08:18:54 +0100

    BtnOn/Off Styling

--- web-app/src/component/HomePageButton.js
+++ web-app/src/component/HomePageButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import '../css/Button.css';
 
 const HomePageButton = ({ label, onClick, disabled, icon }) => {
@@ -8,6 +8,37 @@ const HomePageButton = ({ label, onClick, disabled, icon }) => {
       <span className="button-label">{label}</span>
       {icon && <img src={icon} alt="Icon" className="button-icon" />} 
       {onClick}
+    </button>
+  );
+}
+
+export default HomePageButton;*/
+
+import React, { useState } from 'react';
+import '../css/Button.css';
+
+const HomePageButton = ({ label, onClick, disabled, icon }) => {
+  // State to manage whether the button is on or off
+  const [isOn, setIsOn] = useState(false);
+
+
+  const handleClick = () => {
+    setIsOn(!isOn); // change state to the opposite of the current
+    if(onClick) {
+      onClick(); // Call original onClick function if provided
+    }
+  };
+
+  return (
+    <button 
+      className={`button ${disabled ? 'disabled' : ''} ${isOn ? 'on' : ''}`} 
+      onClick={handleClick}
+      disabled={disabled}
+    >
+      <span className="button-label">{label}</span>
+      {icon && <img src={icon} alt="Icon" className="button-icon" />} 
+      {/* Toggle label below */}
+      <div className="button-status-label">{isOn ? 'On' : 'Off'}</div>
     </button>
   );
 }
