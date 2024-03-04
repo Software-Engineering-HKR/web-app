b9e4f97ab0e547caf0e2395af24066843b70a489
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2024-03-04 08:14:23 +0100

    Fixed Icons

--- web-app/src/component/SmartIcons.js
+++ web-app/src/component/SmartIcons.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { PiLampFill, PiLampDuotone, PiDoorDuotone, PiDoorOpenDuotone} from "react-icons/pi";
-import { MdOutlineSensors } from "react-icons/md";
+import { PiLampFill, PiLampDuotone, PiDoorDuotone, PiDoorOpenDuotone, PiFanFill, PiFanDuotone} from "react-icons/pi";
+import { MdOutlineSensors, MdOutlineDoorSliding } from "react-icons/md";
 import { FaDotCircle } from "react-icons/fa";
+import { GiWindow, /*GiClosedDoors*/ } from "react-icons/gi";
 
 function SmartIcons({ device, active }) {
     
     let IconComponent;
 
-    if (device === "whiteLed") {
-        IconComponent = active ? <PiLampFill size={50} color="white" /> : <PiLampDuotone size={50} color="black" />
-    } else if (device === "yellowLed") {
-        IconComponent = active ? <PiLampFill size={50} color="yellow" /> : <PiLampDuotone size={50} color="#D0A103" />
-    } else if (device === "door") {
-        IconComponent = active ? <PiDoorOpenDuotone size={50} color="white" /> : <PiDoorDuotone size={50} color="black" />
-    } else if (device === "motionSensor") {
-        IconComponent = active ? <MdOutlineSensors size={50} className="card-icon-top" /> : <FaDotCircle size={10} className="card-icon-top" />
-    } else {
-        IconComponent = <PiLampDuotone size={50} color="black" />
-    } 
+    switch (device) {
+        case "led":
+            IconComponent = active ? <PiLampFill size={50} color="white" /> : <PiLampDuotone size={50} color="black" />;
+            break;
+        case "yellowLed":
+            IconComponent = active ? <PiLampFill size={50} color="yellow" /> : <PiLampDuotone size={50} color="#D0A103" />;
+            break;
+        case "door":
+            IconComponent = active ? <PiDoorOpenDuotone size={50} color="white" /> : <PiDoorDuotone size={50} color="black" />;
+            break;
+        case "window":
+            IconComponent = active ? <GiWindow size={50} color="white" /> : <MdOutlineDoorSliding size={50} color="black" />;
+            break;
+        case "fan":
+            IconComponent = active ? <PiFanFill size={50} color="white" /> : <PiFanDuotone size={50} color="black" />;
+            break;
+        case "motion":
+            IconComponent = active ? <MdOutlineSensors size={50} className="card-icon-top" /> : <FaDotCircle size={10} className="card-icon-top" />;
+            break;
+        default:
+            IconComponent = active ? <PiLampFill size={50} color="white" /> : <PiLampDuotone size={50} color="black" />;
+    }
+    
 
     return (
         <>
