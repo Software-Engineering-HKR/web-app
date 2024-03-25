239aaea39f2d92ddb8822c2e7616cb8ba2509a6f
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2024-03-25 11:08:13 +0100

    Redesign Devices

--- web-app/src/component/DeviceToggle.js
+++ web-app/src/component/DeviceToggle.js
@@ -5,15 +5,18 @@ import ToggleButton from 'react-bootstrap/ToggleButton';
 import SmartIcons from './SmartIcons';
 import { SensorContext } from '../context/GlobalState';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function DeviceToggle({ label, device, disabled }) {
     const [checked, setChecked] = useState(false);
     const { sensors } = useContext(SensorContext)
 
     useEffect(() => {
         if (sensors.length === 0) return;
-        
-        const deviceInital =async  ()=>{
-            const initialData =  sensors
+
+        const deviceInital = async () => {
+            const initialData = sensors
             const deviceStatus = initialData.find(d => d.name === device)?.status ?? 0;
             setChecked(deviceStatus)
         }
@@ -21,11 +24,11 @@ function DeviceToggle({ label, device, disabled }) {
     }, [device, sensors])
 
     const sendDeviceCommand = async (device, command) => {
-      try {
-        await axios.post(`http://localhost:5000/api/${device}`, { command });
-      } catch (error) {
-        console.error('Error sending device command:', error);
-      }
+        try {
+            await axios.post(`http://localhost:5000/api/${device}`, { command });
+        } catch (error) {
+            console.error('Error sending device command:', error);
+        }
     };
 
     return (
@@ -40,17 +43,27 @@ function DeviceToggle({ label, device, disabled }) {
                 onChange={(e) => {
                     sendDeviceCommand(device, checked ? '0' : '1')
                     setChecked(!checked); // Changes the checked state
-                    
+
                 }}
                 disabled={disabled}
-                
-            >
-                <div className="device-content">
-                    <span className="button-label">{label}</span>
-                    <SmartIcons device={device} active={checked} />
-                    <span className="button-status-label">{checked ? 'On' : 'Off'}</span>
-                </div>
 
+            >
+                <Row className="align-items-center justify-content-between w-100 mx-0">
+                    <Col xs={2} md={4} xl={2}>
+                        <SmartIcons className="large-icon" device={device} active={checked} />
+                    </Col>
+                    <Col xs={10} md={8} xl={10}>
+                        <Row className="justify-content-end">
+                            <Col xs={12} className="device-toggle-name text-right">
+                                {label}
+                            </Col>
+                            <Col xs={12} className="device-toggle-status text-right">
+                                {checked ? 'On' : 'Off'}
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                
             </ToggleButton>
         </>
     );
