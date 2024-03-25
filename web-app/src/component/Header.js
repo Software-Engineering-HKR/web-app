3c86bdd0e463817e7718b9dc414fbe2e114e1887
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2024-03-25 12:18:41 +0100

    Make navbar sticky at the top of the page

--- web-app/src/component/Header.js
+++ web-app/src/component/Header.js
@@ -4,13 +4,14 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container>
         <Navbar.Brand href="#home">Smart Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Settings">Settings</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
