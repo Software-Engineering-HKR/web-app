1f71b7a06f5dfa4e46f0247815fe4658045662ed
Yana Zlatanova <35622632+yanazlatanova@users.noreply.github.com>
2024-04-24 14:02:54 +0200

    Fix css file name

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/deviceToggle.css';
 import './css/index.css';
-import './css/Sensor.css';
+import './css/sensor.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
