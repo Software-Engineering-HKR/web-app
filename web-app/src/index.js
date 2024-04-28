ee9a963297e7cd9990825ab29e018308c61edf92
Asom98 <kaseemsh985@gmail.com>
2024-04-28 14:37:29 +0200

    organizing and matching the design

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/deviceToggle.css';
 import './css/index.css';
-import './css/sensor.css';
+import './css/Sensor.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- web-app/src/pages/User.js
+++ web-app/src/pages/User.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from 'react'
 import { SensorContext } from '../context/GlobalState'
 import { useNavigate } from "react-router-dom";
 import Header from '../component/Header';
+import '../css/Profile.css';
+
 
 
 const User = () => {
@@ -17,23 +19,17 @@ const User = () => {
   }
 
   return (
-    <>
+        <>
       <Header></Header>
-      <div className="container mt-5">
-      <div className="card shadow-lg">
-        <div className="card-header bg-primary text-white">
-          <h5 className="card-title">User Profile</h5>
-        </div>
-        <div className="card-body">
-          <h6 className="card-subtitle mb-2 text-muted">Welcome back!</h6>
-          <p className="card-text">Username: {user?.username}</p>
-          <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
-        </div>
-        <div className="card-footer text-muted">
-          Logged in as {user?.username}
+      <div className="container mt-5 d-flex justify-content-center">
+        <div className="card shadow-lg bg-lightgray border-light" style={{ position: "relative", width: "400px" }}>
+          <div className="card-body">
+            <h5 className="card-title ">Profile</h5>
+            <p className="card-text">Username: {user?.username}</p>
+            <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+          </div>
         </div>
       </div>
-    </div>
     </>
 
   )
