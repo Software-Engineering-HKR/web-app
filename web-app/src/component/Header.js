f36c0f01c82e8e1cd880e77f6e19405f30c46082
Asom98 <kaseemsh985@gmail.com>
2024-04-28 14:55:26 +0200

    adding the icon to the header and moving the profile name to the right

--- web-app/src/component/Header.js
+++ web-app/src/component/Header.js
@@ -1,23 +1,27 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { SensorContext } from '../context/GlobalState';
+import homeIcon from '../assets/home-icon1.png';
 
 function Header() {
-
-  const { user } = useContext(SensorContext)
-
+  const { user } = useContext(SensorContext);
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">Smart Home</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={homeIcon} alt="Home Icon" width="30" height="30" className="d-inline-block align-top" />
+          {' Smart Home'}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/Home">Home</Nav.Link>
-            <Nav.Link href="/user">{user?.username}</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/user" className="ml-auto">{user?.username}</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
